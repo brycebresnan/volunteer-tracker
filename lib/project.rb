af28fed303aea7f30fd314f339620cde47f1cf14
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-10-07 13:18:23 -0700

    create == method

--- lib/project.rb
+++ lib/project.rb
@@ -6,5 +6,13 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
+  end
   
+  # def save
+  #   result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  #   @id = result.first().fetch('id').to_i
+  # end
 end
