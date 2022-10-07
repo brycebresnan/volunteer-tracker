ecfc16798e281e6c4bf5f3e31346a760cfc28449
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-10-07 13:58:10 -0700

    creat save method. passes self.all test also

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -18,10 +18,15 @@ class Volunteer
     results.each do |vol|
       id = vol.fetch('id').to_i
       name = vol.fetch('name')
-      project_id = vol.getch('project_id').to_i
+      project_id = vol.fetch('project_id').to_i
       volunteers.push(Volunteer.new({id: id, name: name, project_id: project_id}))
     end
     volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch('id').to_i
   end
 
 end
