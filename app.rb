5da9204a46bc605e9acdc10ac34e5c00b378d5db
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-10-08 23:07:36 -0700

    add edit routes,views pass update integration test

--- app.rb
+++ app.rb
@@ -15,8 +15,30 @@ get('/') do
   erb(:projects)
 end
 
+get('/projects') do
+  redirect to('/')
+end
+
 post('/projects') do
   project = Project.new({id: nil, title: params['project_title']})
   project.save()
   redirect to('/')
+end
+
+patch('/projects/:id') do
+  project = Project.find(params[:id].to_i)
+  project.update({title: params[:project_title], id: nil})
+  binding.pry
+  redirect to('/')
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i)
+  @volunteers = Volunteer.all
+  erb(:project_details)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i)
+  erb(:project_edit)
 end
